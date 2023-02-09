bf3fae51a32f99633929452f16b8f220dfdff720
Wei He <hewe@microsoft.com>
2023-02-09 21:49:11 +0800

    work on scmp

---
+++ opinionnews/opinionnews/spiders/scmp.py
@@ -0,0 +1,54 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+import scrapy
+import datetime
+
+from scrapy.loader import ItemLoader
+from ..items import OpinionNewsItem
+
+class BloombergSpider(scrapy.Spider):
+    name = "bloomberg"
+    base_url = "https://www.scmp.com"
+    allowed_domains = ['scmp.com']
+    start_urls = [
+        'https://www.scmp.com/comment/insight-opinion'
+    ]
+
+    def parse(self, response):
+        now = datetime.datetime.now()
+        nowTS = int(now.timestamp())
+        
+        for article in response.xpath('//article[contains(@class, "story-list-story")]'):
+            
+            title = article.xpath('.//div[contains(@class, "headline")]/a/text()').get()
+            summary = article.xpath('.//div[contains(@class, "summary")]/p/text()').get()
+            url = article.xpath('.//div[contains(@class, "headline")]/a/@href').get()
+            thumbnail = article.xpath('.//a[contains(@class, "image")]/img/@src').get()
+            author = article.xpath('.//span[contains(@class, "byline")]/text()').get()
+            """
+            article //div[contains(@class, "article-level")]
+
+url = response.xpath('//div[contains(@class, "article-level")]//div/a/@href').getall()
+            url = response.xpath('//article[contains(@class, "article-story")]/div[contains(@class, "thumbnail")]/a/@href').getall()
+thumbnail = response.xpath('//article[contains(@class, "article-story")]/div[contains(@class, "thumbnail")]//img/@src').getall()
+
+title = response.xpath('//article[contains(@class, "article-story")]/div[contains(@class, "details")]//p/text()').getall()
+            """
+            yield OpinionNewsItem(title=title.strip('\n '), 
+                                  summary=summary, 
+                                  url=self.base_url + url, 
+                                  thumbnail=thumbnail, 
+                                  author=author,
+                                  source=self.name,
+                                  updateDate=nowTS)
+            #yield {"title": title, 
+            #       "summary": summary, 
+            #       "url": url, 
+            #       "thumbnail": thumbnail, 
+            #       "author": author, 
+            #       "crawlDate": nowTS}
+            #item = l.load_item()
+            #item.crawlDate = nowTS
+            #yield item
